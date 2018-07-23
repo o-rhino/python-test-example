0b11d7ec2c6c68bf235552caee5e6d45edc2bedc
Daniel Palmer <dpalmer@winemantech.com>
2018-07-23 15:54:25 -0400

    trying things with the shell

--- runUnittestShell.sh
+++ runUnittestShell.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
-c:\cygwin64\bin\find . -name "*Test.py" -print | while read f; do
-        echo "$f"
+#c:\cygwin64\bin\find . -name "*Test.py" -print | while read f; do
+#        echo "$f"
         ###
-        python3 -m coverage run "$f"
-        python3 -m coverage xml -o coverage.xml
+#        python3 -m coverage run "$f"
+#        python3 -m coverage xml -o coverage.xml
         ###
-done
-c:\cygwin64\bin\cp -r ./coverage.xml /var/lib/jenkins/workspace/example/coverage.xml
-c:\cygwin64\bin\cp -r ./python_unittests_xml /var/lib/jenkins/workspace/example/python_unittests_xml
+#done
+#c:\cygwin64\bin\cp -r ./coverage.xml /var/lib/jenkins/workspace/example/coverage.xml
+#c:\cygwin64\bin\cp -r ./python_unittests_xml /var/lib/jenkins/workspace/example/python_unittests_xml
+c:\cygwin64\bin\find . -name "*Test.py" -print
