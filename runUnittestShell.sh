e80e9eb397c5d6defe743f00fab7d673ecec604e
Daniel Palmer <dpalmer@winemantech.com>
2018-07-23 15:55:45 -0400

    still tweaking

--- runUnittestShell.sh
+++ runUnittestShell.sh
@@ -8,4 +8,4 @@
 #done
 #c:\cygwin64\bin\cp -r ./coverage.xml /var/lib/jenkins/workspace/example/coverage.xml
 #c:\cygwin64\bin\cp -r ./python_unittests_xml /var/lib/jenkins/workspace/example/python_unittests_xml
-c:\cygwin64\bin\find . -name "*Test.py" -print
+/usr/bin/find . -name "*Test.py" -print
