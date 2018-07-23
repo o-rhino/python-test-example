66dcfd70d795a09e96335a781ea4ce87da8b2a4d
Daniel Palmer <dpalmer@winemantech.com>
2018-07-23 16:09:19 -0400

    still trying

--- runUnittestShell.sh
+++ runUnittestShell.sh
@@ -1,27 +1,5 @@
-#!/usr/bin/env bash
 /usr/bin/find . -name "*Test.py" -print | while read f; do
         echo "$f"
-        ###
         python -m coverage run "$f"
         python -m coverage xml -o coverage.xml
-        ###
 done
-#
-#/usr/bin/cp -r coverage.xml /var/lib/jenkins/workspace/example/coverage.xml
-#/usr/bin/cp -r python_unittests_xml /var/lib/jenkins/workspace/example/python_unittests_xml
-#
-#!/usr/bin/env bash
-#c:\cygwin64\bin\find . -name "*Test.py" -print | while read f; do
-#        echo "$f"
-        ###
-#        python3 -m coverage run "$f"
-#        python3 -m coverage xml -o coverage.xml
-        ###
-#done
-#c:\cygwin64\bin\cp -r ./coverage.xml /var/lib/jenkins/workspace/example/coverage.xml
-#c:\cygwin64\bin\cp -r ./python_unittests_xml /var/lib/jenkins/workspace/example/python_unittests_xml
-#/usr/bin/find . -name "*Test.py" -print | while read f; do
-#	echo "$f"
-#	python -m coverage run "$f"
-#	python -m coverage xml -o coverage.xml
-#done
