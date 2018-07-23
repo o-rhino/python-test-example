30e762a34e44a338b45cbf8b688fb53176c85bac
Daniel Palmer <dpalmer@winemantech.com>
2018-07-23 15:28:25 -0400

    tweaking shell file for windows

--- runUnittestShell.sh
+++ runUnittestShell.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 c:\cygwin64\bin\find . -name "*Test.py" -print | while read f; do
         echo "$f"
         ###
@@ -7,6 +6,5 @@ c:\cygwin64\bin\find . -name "*Test.py" -print | while read f; do
         python3 -m coverage xml -o coverage.xml
         ###
 done
-
 c:\cygwin64\bin\cp -r ./coverage.xml /var/lib/jenkins/workspace/example/coverage.xml
 c:\cygwin64\bin\cp -r ./python_unittests_xml /var/lib/jenkins/workspace/example/python_unittests_xml
